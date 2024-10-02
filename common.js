2487b1e1b5aa0b1e1b15529716c155d8ba7a659b
SimonSchneider <dev@simonschneider.org>
2024-10-02 16:51:12 +0200

    addd prettier and oxlint

--- common.js
+++ common.js
@@ -1,66 +1,66 @@
 export function getControls(containerId) {
-    const container = document.getElementById(containerId);
-    const inputs = container.querySelectorAll('input, select')
-    return Array.from(inputs).reduce((acc, input) => {
-        return Object.defineProperty(acc, input.id, {
-            get() {
-                const t = input.type;
-                switch (t) {
-                    case 'checkbox':
-                        return input.checked;
-                    case 'number':
-                    case 'range':
-                        return Number(input.value);
-                    default:
-                        return input.value;
-                }
-            }
-        })
-    }, {});
+  const container = document.getElementById(containerId);
+  const inputs = container.querySelectorAll('input, select');
+  return Array.from(inputs).reduce((acc, input) => {
+    return Object.defineProperty(acc, input.id, {
+      get() {
+        const t = input.type;
+        switch (t) {
+          case 'checkbox':
+            return input.checked;
+          case 'number':
+          case 'range':
+            return Number(input.value);
+          default:
+            return input.value;
+        }
+      },
+    });
+  }, {});
 }
 
 export function fileInputSource(inputId, callback) {
-    document.getElementById(inputId).addEventListener('change', async (e) => {
-        const files = e.target.files;
-        if (!files.length) {
-            return;
-        }
-        for (const file of files) {
-            callback(file);
-        }
-    });
+  document.getElementById(inputId).addEventListener('change', async (e) => {
+    const files = e.target.files;
+    if (!files.length) {
+      return;
+    }
+    for (const file of files) {
+      callback(file);
+    }
+  });
 }
 
 export function downloadElem() {
-    const a = document.createElement('a');
-    a.textContent = 'Download';
-    a.setAttribute("class", "disabled");
-    return a;
+  const a = document.createElement('a');
+  a.textContent = 'Download';
+  a.setAttribute('class', 'disabled');
+  return a;
 }
 
 export function downloadReady(elem, blob, name, newExt, autoDownload) {
-    elem.href = URL.createObjectURL(blob);
-    elem.setAttribute("class", "");
-    elem.download = name.slice(0, name.lastIndexOf(".") + 1) + newExt;
-    if (autoDownload) {
-        elem.click();
-    }
+  elem.href = URL.createObjectURL(blob);
+  elem.setAttribute('class', '');
+  elem.download = name.slice(0, name.lastIndexOf('.') + 1) + newExt;
+  if (autoDownload) {
+    elem.click();
+  }
 }
 
 export function getFileList(listId) {
-    const fileListElem = document.getElementById(listId);
-    return {
-        add(file) {
-            const fileElem = document.createElement('li');
-            const nameElem = fileElem.appendChild(document.createElement('span'));
-            nameElem.textContent = file.name;
-            const aElem = fileElem.appendChild(downloadElem());
-            fileListElem.appendChild(fileElem);
-            return {
-                downloadReady: (blob, newExt, autoDownload) => {
-                    downloadReady(aElem, blob, file.name, newExt, autoDownload);
-                }
-            }
-        }
-    }
+  const fileListElem = document.getElementById(listId);
+  return {
+    add(file) {
+      const fileElem = document.createElement('li');
+      const nameElem = fileElem.appendChild(document.createElement('span'));
+      nameElem.textContent = file.name;
+      const aElem = fileElem.appendChild(downloadElem());
+      fileListElem.appendChild(fileElem);
+      return {
+        downloadReady: (blob, newExt, autoDownload) => {
+          downloadReady(aElem, blob, file.name, newExt, autoDownload);
+        },
+      };
+    },
+  };
 }
--- main.js
+++ main.js
@@ -1,28 +1,28 @@
 import heicConvert from 'heic-convert/browser.js';
-import {fileInputSource, getFileList, getControls} from "./common.js";
+import { fileInputSource, getFileList, getControls } from './common.js';
 
 const fileList = getFileList('fileList');
 const controls = getControls('controls');
 
 function convertHEIC(arrayBuffer, format) {
-    const inputBuffer = new Uint8Array(arrayBuffer);
-    return heicConvert({
-        buffer: inputBuffer,
-        format: format.toUpperCase(),
-        quality: controls.quality,
-    });
+  const inputBuffer = new Uint8Array(arrayBuffer);
+  return heicConvert({
+    buffer: inputBuffer,
+    format: format.toUpperCase(),
+    quality: controls.quality,
+  });
 }
 
 function handleFile(file) {
-    const fileEntry = fileList.add(file);
-    const reader = new FileReader();
-    reader.onload = async () => {
-        const format = controls.format;
-        const outputBuffer = await convertHEIC(reader.result, format);
-        const blob = new Blob([outputBuffer], {type: `image/${format}`});
-        fileEntry.downloadReady(blob, format, controls.autoDownload);
-    };
-    reader.readAsArrayBuffer(file);
+  const fileEntry = fileList.add(file);
+  const reader = new FileReader();
+  reader.onload = async () => {
+    const format = controls.format;
+    const outputBuffer = await convertHEIC(reader.result, format);
+    const blob = new Blob([outputBuffer], { type: `image/${format}` });
+    fileEntry.downloadReady(blob, format, controls.autoDownload);
+  };
+  reader.readAsArrayBuffer(file);
 }
 
 fileInputSource('fileInput', handleFile);
